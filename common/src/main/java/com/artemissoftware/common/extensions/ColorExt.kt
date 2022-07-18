076a1f6e2be04b7fec08d13cc087df6779b6c745
gustavo <gustavo.isaac@gmail.com>
2022-07-18 22:07:24 +0100

    ColorExt

---
+++ common/src/main/java/com/artemissoftware/common/extensions/ColorExt.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.common.extensions
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.unit.Dp
+import kotlin.math.ln
+
+
+fun Color.withElevation(elevation: Dp): Color {
+    val foreground = calculateForeground(elevation = elevation)
+    return foreground.compositeOver(this)
+}
+
+private fun calculateForeground(elevation: Dp) : Color {
+    val alpha = ((4.5f * ln(elevation.value + 1)) + 2f) / 100f
+    return Color.White.copy(alpha = alpha)
+}
