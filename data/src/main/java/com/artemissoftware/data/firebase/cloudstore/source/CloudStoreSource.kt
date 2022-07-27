ad1ba0e9095287a3ba98038868ff7d9e9fe00744
gustavo <gustavo.isaac@gmail.com>
2022-07-27 17:56:58 +0100

    CloudStoreSource

---
+++ data/src/main/java/com/artemissoftware/data/firebase/cloudstore/source/CloudStoreSource.kt
@@ -0,0 +1,22 @@
+package com.artemissoftware.data.firebase.cloudstore.source
+
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class CloudStoreSource(private val firebaseFirestore: FirebaseFirestore){
+
+    suspend fun getDocuments(path: String): List<DocumentSnapshot> {
+        return suspendCoroutine { continuation ->
+            firebaseFirestore.collection(path).get()
+                .addOnSuccessListener {
+                    continuation.resume(it.documents)
+                }
+                .addOnFailureListener {
+                    continuation.resumeWithException(it)
+                }
+        }
+    }
+}
