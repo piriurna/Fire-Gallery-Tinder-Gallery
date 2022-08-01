81b4a26ff5955966b305d5ed6204eab3c7a3133f
gustavo <gustavo.isaac@gmail.com>
2022-08-01 18:14:56 +0100

    Dagger dependencies

--- data/src/main/java/com/artemissoftware/data/firebase/cloudstore/source/CloudStoreSource.kt
+++ data/src/main/java/com/artemissoftware/data/firebase/cloudstore/source/CloudStoreSource.kt
@@ -2,11 +2,12 @@ package com.artemissoftware.data.firebase.cloudstore.source
 
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class CloudStoreSource(private val firebaseFirestore: FirebaseFirestore){
+class CloudStoreSource @Inject constructor(private val firebaseFirestore: FirebaseFirestore){
 
     suspend fun getDocuments(path: String): List<DocumentSnapshot> {
         return suspendCoroutine { continuation ->
