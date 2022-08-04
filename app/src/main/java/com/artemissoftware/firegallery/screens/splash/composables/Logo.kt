404466b5a352c7911e7c49120917b5bf6f9cb722
gustavo <gustavo.isaac@gmail.com>
2022-08-04 19:34:33 +0100

    Logo

---
+++ app/src/main/java/com/artemissoftware/firegallery/screens/splash/composables/Logo.kt
@@ -0,0 +1,54 @@
+package com.artemissoftware.firegallery.screens.splash.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.common.theme.Orange
+import com.artemissoftware.common.theme.RedOrange
+import com.artemissoftware.firegallery.R
+import com.artemissoftware.firegallery.screens.SplashScreen
+
+@Composable
+fun Logo() {
+
+    Box(
+        modifier = Modifier
+            .size(80.dp)
+            .clip(CircleShape)
+            .background(color = Orange)
+            .border(
+                width = 2.dp,
+                color = RedOrange,
+                shape = CircleShape
+            )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_flame),
+            contentDescription = "Compose image",
+            colorFilter =  ColorFilter.tint(color = Red),
+            modifier = Modifier
+                .size(56.dp)
+                .align(alignment = Alignment.Center)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LogoPreview() {
+
+    Logo()
+}
--- common/src/main/java/com/artemissoftware/common/theme/FGColor.kt
+++ common/src/main/java/com/artemissoftware/common/theme/FGColor.kt
@@ -7,3 +7,8 @@ import androidx.compose.ui.graphics.Color
 @get:Composable
 val Colors.secondaryBackground: Color
     get() = if (isLight) Color(0xffffffff) else Color(0xff252525)
+
+
+val Orange = Color(0xFFFFC55C)
+val Red = Color(0xFFFC2E20)
+val RedOrange = Color(0xFFFF4500)
