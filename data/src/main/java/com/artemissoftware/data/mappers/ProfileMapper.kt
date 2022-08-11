df0808bf2d6b030ac81dadcd930c5f5da843d915
gustavo <gustavo.isaac@gmail.com>
2022-08-11 14:58:50 +0100

    ProfileMapper

---
+++ data/src/main/java/com/artemissoftware/data/mappers/ProfileMapper.kt
@@ -0,0 +1,8 @@
+package com.artemissoftware.data.mappers
+
+import com.artemissoftware.data.local.models.ProfileSettings
+import com.artemissoftware.domain.models.profile.Profile
+
+fun ProfileSettings.toProfile() : Profile{
+    return Profile(notifications = this.notifications)
+}
