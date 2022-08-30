e192781486e281d1e37cd710b1c7541cf703e1f1
gustavo <gustavo.isaac@gmail.com>
2022-08-30 13:16:17 +0100

    Code clean up

--- app/src/main/java/com/artemissoftware/firegallery/MainEvents.kt
+++ app/src/main/java/com/artemissoftware/firegallery/MainEvents.kt
@@ -2,6 +2,4 @@ package com.artemissoftware.firegallery
 
 import com.artemissoftware.firegallery.ui.FGBaseEvents
 
-sealed class MainEvents: FGBaseEvents() {
-
-}
+sealed class MainEvents: FGBaseEvents()
