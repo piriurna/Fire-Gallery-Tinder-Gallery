3eebdaf8709794cc1ce9807dfb422716e8fe2d6d
gustavo <gustavo.isaac@gmail.com>
2022-09-11 18:53:43 +0100

    FGCircularButton

---
+++ common/src/main/java/com/artemissoftware/common/composables/button/FGCircularButton.kt
@@ -0,0 +1,101 @@
+package com.artemissoftware.common.composables.button
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.artemissoftware.common.R
+
+@Composable
+fun FGCircularButton(
+    imageVector: ImageVector,
+    modifier: Modifier = Modifier,
+    clickListener: () -> Unit = {}
+) {
+    Surface(
+        shape = CircleShape,
+        modifier = modifier.size(48.dp),
+        elevation = 4.dp, // play with the elevation values
+    ) {
+
+
+        Icon(
+            imageVector = imageVector,
+            contentDescription = "", tint=Color(0XFF0F9D58)
+        )
+    }
+}
+
+@Composable
+fun FGCircularButton(
+    @DrawableRes resId: Int,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.primary,
+    contentScale: ContentScale = ContentScale.Fit,
+    clickListener: () -> Unit = {}
+) {
+    Surface(
+        shape = CircleShape,
+        modifier = modifier.size(48.dp),
+        elevation = 4.dp, // play with the elevation values
+    ) {
+
+
+
+        Image(
+            painter = painterResource(id = resId),
+            contentDescription = null, // decorative
+            contentScale = contentScale,
+            colorFilter  = ColorFilter.tint(color),
+
+            )
+    }
+}
+
+@Preview
+@Composable
+private fun FGCircularButtonPreview() {
+
+    Column {
+
+        FGCircularButton(
+            imageVector = Icons.Default.KeyboardArrowLeft,
+            modifier = Modifier
+                //.align(Alignment.TopEnd)
+                .padding(16.dp)
+        ){}
+
+
+        FGCircularButton(
+            resId = R.drawable.ic_android,
+        ){}
+    }
+
+
+}
+
