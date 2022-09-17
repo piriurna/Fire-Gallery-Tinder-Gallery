6c34eed8635c092ae21dc113e261561959c98dcb
gustavo <gustavo.isaac@gmail.com>
2022-09-17 19:03:57 +0100

    FGTopBar - animation part 3

--- common/src/main/java/com/artemissoftware/common/composables/topbar/FGTopBar.kt
+++ common/src/main/java/com/artemissoftware/common/composables/topbar/FGTopBar.kt
@@ -2,6 +2,7 @@ package com.artemissoftware.common.composables.topbar
 
 import androidx.annotation.DrawableRes
 import androidx.compose.animation.*
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
@@ -9,6 +10,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -17,14 +19,18 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import com.artemissoftware.common.composables.button.FGCircularButton
 import com.artemissoftware.common.composables.chip.Purple200
@@ -184,6 +190,7 @@ fun FGTopBar(
     optionComposable: (@Composable BoxScope.() -> Unit)? = null,
 ) {
 
+    val animationTime = 300
 
     AppBar(
         modifier = modifier,
@@ -192,7 +199,13 @@ fun FGTopBar(
 
         AnimatedVisibility(
             visible = isVisible,
-            enter = fadeIn(),
+            enter = slideInHorizontally(
+                initialOffsetX = { -3000 }, // small slide 300px
+                animationSpec = tween(
+                    durationMillis = animationTime,
+                    easing = LinearEasing // interpolator
+                )
+            ),
             exit = fadeOut(),
             modifier = Modifier,
         ) {
@@ -312,59 +325,59 @@ fun FGTopBar(
 }
 
 
-
-@Preview(showBackground = true)
-@Composable
-private fun FGTopBar_1_Preview() {
-
-    FGTopBar(
-        onNavigationClick = {},
-        optionComposable = {}
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun FGTopBar_2_Preview() {
-
-    FGTopBar(
-        title = "Title",
-        subTitle = "subTitle",
-        onNavigationClick = {},
-        optionComposable = {}
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun FGTopBar_3_Preview() {
-
-    FGTopBar(
-        onNavigationClick = {},
-        optionComposable = {
-            FGCircularButton(
-                imageVector = Icons.Default.KeyboardArrowLeft,
-                modifier = Modifier.align(Alignment.CenterEnd)
-            )
-        }
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun FGTopBar_4_Preview() {
-
-    FGTopBar(
-        title = "Title",
-        onNavigationClick = {},
-        optionComposable = {
-            FGCircularButton(
-                imageVector = Icons.Default.KeyboardArrowLeft,
-                modifier = Modifier.align(Alignment.CenterEnd)
-            )
-        }
-    )
-}
+//
+//@Preview(showBackground = true)
+//@Composable
+//private fun FGTopBar_1_Preview() {
+//
+//    FGTopBar(
+//        onNavigationClick = {},
+//        optionComposable = {}
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//private fun FGTopBar_2_Preview() {
+//
+//    FGTopBar(
+//        title = "Title",
+//        subTitle = "subTitle",
+//        onNavigationClick = {},
+//        optionComposable = {}
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//private fun FGTopBar_3_Preview() {
+//
+//    FGTopBar(
+//        onNavigationClick = {},
+//        optionComposable = {
+//            FGCircularButton(
+//                imageVector = Icons.Default.KeyboardArrowLeft,
+//                modifier = Modifier.align(Alignment.CenterEnd)
+//            )
+//        }
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//private fun FGTopBar_4_Preview() {
+//
+//    FGTopBar(
+//        title = "Title",
+//        onNavigationClick = {},
+//        optionComposable = {
+//            FGCircularButton(
+//                imageVector = Icons.Default.KeyboardArrowLeft,
+//                modifier = Modifier.align(Alignment.CenterEnd)
+//            )
+//        }
+//    )
+//}
 
 
 @Stable
@@ -389,11 +402,117 @@ enum class AlertState {
     Expanded
 }
 
+//@Preview
+//@Composable
+//fun HomeScreen() {
+//    val scope = rememberCoroutineScope()
+//    val alertHeight = 40.dp
+//    var currentState by remember { mutableStateOf(AlertState.Collapsed) }
+//    val transition = updateTransition(currentState, label = "")
+//    val alertOffset by transition.animateDp(label = "") {
+//        when (it) {
+//            AlertState.Collapsed -> -alertHeight
+//            AlertState.Expanded -> 0.dp
+//        }
+//    }
+//    Column(Modifier.fillMaxHeight()) {
+//
+//        Column(
+//            modifier = Modifier.clipToBounds().offset(y = alertOffset)
+//        ) {
+//            Row(
+//                Modifier.height(alertHeight).fillMaxWidth().background(Color.Red).padding(start = 16.dp)
+//            ) {
+//                Text(
+//                    "An error has occurred", style = TextStyle(Color.White),
+//                    modifier = Modifier.align(Alignment.CenterVertically)
+//                )
+//            }
+//            Button(modifier = Modifier.padding(16.dp), onClick = {
+//                scope.launch {
+//                    //currentState = AlertState.Expanded
+//                    delay(100)
+//                    currentState = if(currentState == AlertState.Expanded) AlertState.Collapsed else AlertState.Expanded
+//                }
+//            }) {
+//                Text("Show Alert" + "  - " + alertOffset.value + "...." + currentState)
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@ExperimentalAnimationApi
+//@Composable
+//fun MainContent(){
+//    val longtext = "Lorem Ipsum is simply dummy text of the printing" +
+//            " and typesetting industry. Lorem Ipsum has been the" +
+//            " industry's standard dummy text ever since the 1500s," +
+//            " when an unknown printer took a galley of type and" +
+//            " scrambled it to make a type specimen book."
+//
+//    val isVisible = remember { mutableStateOf(true) }
+//
+//    Column(
+//        modifier = Modifier
+//            .background(Color(0xFFF5F5F5))
+//            .padding(16.dp)
+//            .fillMaxSize(),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//    ){
+//        Button(
+//            onClick = {
+//                isVisible.value = !isVisible.value
+//            },
+//        ) {
+//            Text(if (isVisible.value)"Hide" else "Show")
+//        }
+//        val animationTime = 300
+//
+//        AnimatedVisibility(
+//            visible = isVisible.value,
+//            enter = slideInHorizontally(
+//                initialOffsetX = { -3000 }, // small slide 300px
+//                animationSpec = tween(
+//                    durationMillis = animationTime,
+//                    easing = LinearEasing // interpolator
+//                )
+//            ),
+//            exit = fadeOut(),
+//        ) {
+//            Box(
+//                modifier = Modifier
+//                    .clip(RoundedCornerShape(12.dp))
+//                    .background(Color(0xFFD2691E))
+//                    .padding(16.dp)
+//                    .fillMaxSize()
+//            ) {
+//                Text(
+//                    text = longtext,
+//                    fontSize = 35.sp,
+//                    fontWeight = FontWeight.Bold,
+//                    textAlign = TextAlign.Center,
+//                    fontFamily = FontFamily.Cursive,
+//                    color = Color(0xFF58111A),
+//                    modifier = Modifier.align(Alignment.Center)
+//                )
+//            }
+//        }
+//    }
+//}
+
+
+
+
+//------------------------------------------------------------------------
+
+
+
 @Preview
 @Composable
 fun HomeScreen() {
     val scope = rememberCoroutineScope()
-    val alertHeight = 40.dp
+    val alertHeight = 56.dp
     var currentState by remember { mutableStateOf(AlertState.Collapsed) }
     val transition = updateTransition(currentState, label = "")
     val alertOffset by transition.animateDp(label = "") {
@@ -403,29 +522,135 @@ fun HomeScreen() {
         }
     }
     Column(Modifier.fillMaxHeight()) {
+//
+//        Column(
+//            modifier = Modifier
+//                .clipToBounds()
+//                .offset(y = alertOffset)
+//        ) {
+//
+//            AppBar(
+//                backgroundColor = Color.Red,
+//                modifier = Modifier.height(alertHeight)
+//            ) {
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxHeight(),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    FGCircularButton(
+//                        imageVector = Icons.Default.KeyboardArrowLeft,
+//                        modifier = Modifier,
+//                        onClick = {  }
+//                    )
+//
+//                    Column(
+//                        modifier = Modifier
+//                            .fillMaxHeight()
+//                            .weight(0.5F)
+//                            .padding(start = 16.dp),
+//                        horizontalAlignment = Alignment.Start,
+//                        verticalArrangement = Arrangement.Center
+//                    ) {
+//
+//                        "title"?.let {
+//
+//                            FGText(
+//                                text = it,
+//                                style = FGStyle.TextAlbertSansBold16,
+//                                textAlign = TextAlign.Start
+//                            )
+//                        }
+//
+//
+//                        "subTitle"?.let {
+//                            FGText(
+//                                text = it,
+//                                style = FGStyle.TextAlbertSansMedium,
+//                                textAlign = TextAlign.Start
+//                            )
+//                        }
+//                    }
+//
+//
+////                    Box(modifier = Modifier.weight(0.1F)) {
+////                        optionComposable?.let {
+////                            it()
+////                        }
+////                    }
+//
+//
+//                }
+//            }
+//
+//
+//        }
 
-        Column(
-            modifier = Modifier.clipToBounds().offset(y = alertOffset)
+        AppBar(
+            backgroundColor = Color.Red,
+            modifier = Modifier.padding(top = 20.dp)
         ) {
             Row(
-                Modifier.height(alertHeight).fillMaxWidth().background(Color.Red).padding(start = 16.dp)
+                modifier = Modifier
+                    .fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(
-                    "An error has occurred", style = TextStyle(Color.White),
-                    modifier = Modifier.align(Alignment.CenterVertically)
+                FGCircularButton(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    modifier = Modifier.offset(y = alertOffset),
+                    onClick = {  }
                 )
-            }
-            Button(modifier = Modifier.padding(16.dp), onClick = {
-                scope.launch {
-//                    currentState = AlertState.Expanded
-//                    delay(1000)
-//                    currentState = AlertState.Collapsed
 
-                    currentState = if(currentState == AlertState.Collapsed) AlertState.Expanded else AlertState.Collapsed
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.5F)
+                        .padding(start = 16.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    "title"?.let {
+
+                        FGText(
+                            text = it,
+                            style = FGStyle.TextAlbertSansBold16,
+                            textAlign = TextAlign.Start
+                        )
+                    }
+
+
+                    "subTitle"?.let {
+                        FGText(
+                            text = it,
+                            style = FGStyle.TextAlbertSansMedium,
+                            textAlign = TextAlign.Start
+                        )
+                    }
                 }
-            }) {
-                Text("Show Alert")
+
+
+//                    Box(modifier = Modifier.weight(0.1F)) {
+//                        optionComposable?.let {
+//                            it()
+//                        }
+//                    }
+
+
             }
+        }
+
+        Button(modifier = Modifier.padding(16.dp), onClick = {
+            scope.launch {
+                //currentState = AlertState.Expanded
+                delay(100)
+                currentState = if(currentState == AlertState.Expanded) AlertState.Collapsed else AlertState.Expanded
+            }
+        }) {
+            Text("Show Alert" + "  - " + alertOffset.value + "...." + currentState)
         }
     }
 }
+
+
+
