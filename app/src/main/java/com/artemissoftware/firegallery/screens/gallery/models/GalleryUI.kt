cd6d458dd6ec38d5aeafff736476d5304b9113cd
gustavo <gustavo.isaac@gmail.com>
2022-09-25 16:04:40 +0100

    GalleryUI

---
+++ app/src/main/java/com/artemissoftware/firegallery/screens/gallery/models/GalleryUI.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.firegallery.screens.gallery.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class GalleryUI(
+    val name: String
+) : Parcelable
