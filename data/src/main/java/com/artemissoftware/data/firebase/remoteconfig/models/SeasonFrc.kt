ba268d48078bf89a1c427389ca5c3aceb7790153
gustavo <gustavo.isaac@gmail.com>
2022-10-08 18:30:35 +0100

    SeasonFrc

---
+++ data/src/main/java/com/artemissoftware/data/firebase/remoteconfig/models/SeasonFrc.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.data.firebase.remoteconfig.models
+
+data class SeasonFrc(
+    val backgroundColor: String,
+    val chipColor: String,
+    val seasonMessage: String? = null
+)
