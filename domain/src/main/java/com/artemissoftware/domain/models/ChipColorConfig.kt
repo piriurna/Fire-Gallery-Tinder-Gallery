05091820907500199d2d204f3e6d4ccb2caf5445
gustavo <gustavo.isaac@gmail.com>
2022-10-10 10:28:33 +0100

    ChipColorConfig

---
+++ domain/src/main/java/com/artemissoftware/domain/models/ChipColorConfig.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.domain.models
+
+data class ChipColorConfig(
+    val startBorderColor: String,
+    val endBorderColor: String,
+    val iconColor: String
+)
--- domain/src/main/java/com/artemissoftware/domain/models/SeasonConfig.kt
+++ domain/src/main/java/com/artemissoftware/domain/models/SeasonConfig.kt
@@ -2,6 +2,6 @@ package com.artemissoftware.domain.models
 
 data class SeasonConfig(
     val backgroundColor: String,
-    val chipColor: String,
+    val chipColor: ChipColorConfig,
     val seasonMessage: String? = null
 )
