450cd5b66ca386bf21dd10194bbc49ac94672ecf
gustavo <gustavo.isaac@gmail.com>
2022-10-19 16:40:09 +0100

    RegisterEvents

---
+++ app/src/main/java/com/artemissoftware/firegallery/screens/register/RegisterEvents.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.firegallery.screens.register
+
+import com.artemissoftware.firegallery.ui.FGBaseEvents
+
+sealed class RegisterEvents: FGBaseEvents() {
+    data class ValidateRegister(val email: String, val password: String, val passwordConfirm: String): RegisterEvents()
+}
