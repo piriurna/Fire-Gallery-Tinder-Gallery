76a0f90584dfd3f91bfdeaa7da7db3b1c2c04138
gustavo <gustavo.isaac@gmail.com>
2022-10-21 15:23:28 +0100

    AuthenticationSource

---
+++ data/src/main/java/com/artemissoftware/data/firebase/cloudstore/source/AuthenticationSource.kt
@@ -0,0 +1,31 @@
+package com.artemissoftware.data.firebase.cloudstore.source
+
+import com.artemissoftware.data.errors.FireGalleryException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+class AuthenticationSource @Inject constructor(private val firebaseAuth: FirebaseAuth) {
+
+    suspend fun loginUser(email: String, password: String) : FirebaseUser {
+
+        return suspendCoroutine { continuation ->
+            firebaseAuth
+                .signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener { task->
+                    task.user?.let {
+                        continuation.resume(it)
+                    } ?: kotlin.run {
+                        continuation.resumeWithException(FireGalleryException(message = "Error authenticating the user"))
+                    }
+                }
+                .addOnFailureListener {
+                    continuation.resumeWithException(it)
+                }
+        }
+    }
+}
+
