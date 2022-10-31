a4e78df7fd1e913970ffd6329db30797dfff9dc4
gustavo <gustavo.isaac@gmail.com>
2022-10-31 08:19:50 +0000

    Combined PictureCard and FavoriteCard

--- app/src/main/java/com/artemissoftware/firegallery/screens/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/artemissoftware/firegallery/screens/favorites/FavoritesScreen.kt
@@ -1,14 +1,14 @@
 package com.artemissoftware.firegallery.screens.favorites
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,8 +23,8 @@ import com.artemissoftware.domain.models.Picture
 import com.artemissoftware.firegallery.R
 import com.artemissoftware.firegallery.navigation.HomeDestinations
 import com.artemissoftware.firegallery.navigation.graphs.GalleryDestinations
-import com.artemissoftware.firegallery.screens.favorites.composables.FavoriteCard
 import com.artemissoftware.firegallery.screens.pictures.PictureState
+import com.artemissoftware.firegallery.screens.pictures.composables.PictureCard
 import com.artemissoftware.firegallery.ui.UIEvent
 import kotlinx.coroutines.flow.collectLatest
 
@@ -87,7 +87,7 @@ private fun BuildFavoritesScreen(
                 state.pictures.forEach { picture ->
 
                     AnimatedVisibility(visible = state.isFavorite(pictureId = picture.id)) {
-                        FavoriteCard(
+                        PictureCard(
                             isFavorite = state.isFavorite(pictureId = picture.id),
                             picture = picture,
                             onFavoriteClick = { pictureId ->
--- app/src/main/java/com/artemissoftware/firegallery/screens/pictures/composables/PictureCard.kt
+++ app/src/main/java/com/artemissoftware/firegallery/screens/pictures/composables/PictureCard.kt
@@ -1,28 +1,62 @@
 package com.artemissoftware.firegallery.screens.pictures.composables
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.artemissoftware.common.composables.FGCard
+import com.artemissoftware.common.composables.animations.models.PulsatingType
 import com.artemissoftware.domain.models.Picture
-import com.artemissoftware.firegallery.screens.favorites.composables.FavoriteCard
+import com.artemissoftware.firegallery.R
+import com.artemissoftware.firegallery.screens.picturedetail.composables.FavoriteButton
 
 @Composable
 fun PictureCard(
     picture: Picture,
     onClick: (String) -> Unit,
+) {
+
+    PictureContent(
+        picture = picture,
+        onClick = onClick
+    )
+}
+
+@Composable
+fun PictureCard(
+    isFavorite: Boolean,
+    picture: Picture,
+    onFavoriteClick: (String) -> Unit,
+    onClick: (String) -> Unit,
+) {
+
+    PictureContent(
+        addFavoriteButton = true,
+        isFavorite = isFavorite,
+        picture = picture,
+        onClick = onClick,
+        onFavoriteClick = onFavoriteClick
+    )
+}
+
+@Composable
+private fun PictureContent(
+    addFavoriteButton: Boolean = false,
+    isFavorite: Boolean = false,
+    picture: Picture,
+    onClick: (String) -> Unit,
+    onFavoriteClick: (String) -> Unit = {},
 ) {
 
     FGCard(
@@ -37,14 +71,38 @@ fun PictureCard(
                 .build()
         )
 
-        Image(
-            painter = painter,
-            contentDescription = "",
-            modifier = Modifier.fillMaxWidth(),
-            contentScale = ContentScale.Crop,
-        )
+        Box {
+
+            Image(
+                painter = painter,
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop,
+            )
+
+            if (addFavoriteButton) {
+
+                FavoriteButton(
+                    pulsatingType = PulsatingType.LIMITED,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(16.dp),
+                    onClickToFavorite = {
+                        onFavoriteClick.invoke(picture.id)
+                    },
+                    onClickToRemoverFavorite = {
+                        onFavoriteClick.invoke(picture.id)
+                    },
+                    isFavorite = isFavorite,
+                )
+            }
+        }
     }
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
@@ -53,4 +111,22 @@ private fun PictureCardPreview() {
         picture = Picture.picturesMockList[0],
         onClick = {}
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FavoriteCardPreview() {
+    PictureCard(
+        isFavorite = true,
+        picture = Picture.picturesMockList[0],
+        onFavoriteClick = {},
+        onClick = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PictureContentPreview() {
+
+    PictureContent(picture = Picture.picturesMockList[0], isFavorite = true, onClick = {})
 }
