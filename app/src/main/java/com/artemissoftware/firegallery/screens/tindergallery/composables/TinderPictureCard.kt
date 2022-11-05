aa0fdf825477397e3ff6ae1633fed8c73ac9d785
Franco <francozalamena@gmail.com>
2022-11-05 22:47:37 +0000

    Tinder Card and buttons

--- app/src/main/java/com/artemissoftware/firegallery/screens/tindergallery/composables/TinderPictureCard.kt
+++ app/src/main/java/com/artemissoftware/firegallery/screens/tindergallery/composables/TinderPictureCard.kt
@@ -1,52 +1,44 @@
 package com.artemissoftware.firegallery.screens.tindergallery.composables
 
-import android.util.Log
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Cancel
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Device
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
-import com.artemissoftware.common.composables.FGCard
-import com.artemissoftware.common.composables.button.FGCircularButton
 import com.artemissoftware.common.composables.images.SwipeCard
 import com.artemissoftware.common.composables.text.FGText
-import com.artemissoftware.common.extensions.rememberSwipeState
+import com.artemissoftware.common.extensions.swipeablecard.SwipeableCardState
+import com.artemissoftware.common.extensions.swipeablecard.rememberSwipeableCardState
 import com.artemissoftware.common.models.SwipeResult
 import com.artemissoftware.common.theme.FGStyle.TextAlbertSansBold28
-import com.artemissoftware.domain.models.Gallery
 import com.artemissoftware.domain.models.Picture
-import com.artemissoftware.firegallery.R
-import java.lang.Float.max
-import java.lang.Float.min
-import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TinderPictureCard(
+    modifier: Modifier = Modifier,
     picture: Picture,
-    onSwiped : (SwipeResult) -> Unit
+    onSwiped : (SwipeResult) -> Unit = {},
+    onDrag : (Offset) -> Unit,
+    swipeState : SwipeableCardState = rememberSwipeableCardState()
 ) {
+    LaunchedEffect(key1 = swipeState.offset.value) {
+        onDrag(swipeState.offset.value)
+    }
 
     val painter = rememberAsyncImagePainter(
         model = ImageRequest.Builder(LocalContext.current)
@@ -56,117 +48,59 @@ fun TinderPictureCard(
             .build()
     )
 
-    var swiped by remember { mutableStateOf(false) }
-
-    val onSwipedLocal = { swipeResult : SwipeResult->
-        swiped = true
-        onSwiped(swipeResult)
-    }
-
-    val actionButtonSize = 48.dp
-
-    if(swiped.not()) {
-        BoxWithConstraints(
-            modifier = Modifier
+    if(swipeState.swipedDirection == null) {
+        Box(
+            modifier = modifier
                 .fillMaxSize(),
         ) {
-            val swipeState = rememberSwipeState(maxWidth = constraints.maxWidth.toFloat(), maxHeight = constraints.maxHeight.toFloat())
-                SwipeCard(
+            SwipeCard(
+                modifier = Modifier
+                    .clickable(enabled = false, onClick = {})
+                    .fillMaxSize(),
+                onSwiped = onSwiped,
+                swipeState = swipeState,
+            ) {
+                Box(
                     modifier = Modifier
-                        .fillMaxSize(),
-                    onSwiped = onSwipedLocal,
-                    swipeState = swipeState
+                        .fillMaxSize()
                 ) {
-                    Box(
+                    Image(
                         modifier = Modifier
-                            .fillMaxSize()
-                    ) {
-                        Image(
-                            modifier = Modifier
-                                .background(Color.Black)
-                                .fillMaxSize(),
-                            painter = painter,
-                            contentDescription = "",
-                            contentScale = ContentScale.Fit,
-                        )
+                            .background(Color.Black)
+                            .fillMaxSize(),
+                        painter = painter,
+                        contentDescription = "",
+                        contentScale = ContentScale.Fit,
+                    )
 
-                        FGText(
-                            text = picture.title,
-                            style = TextAlbertSansBold28,
-                            color = Color.White,
-                            modifier = Modifier
-                                .align(Alignment.BottomCenter)
-                                .padding(bottom = 32.dp, start = 24.dp)
-                        )
+                    FGText(
+                        text = picture.title,
+                        style = TextAlbertSansBold28,
+                        color = Color.White,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 32.dp)
+                    )
 
-                    }
                 }
-
-                FGCircularButton(
-                    modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .graphicsLayer {
-                            val isLikeSwipe = swipeState.offsetX.value < 0f
-                            val targetXPosition = constraints.maxWidth.toFloat() / 2f
-                            val currentXPosition = -min(targetXPosition - 24.dp.toPx(), swipeState.offsetX.value.absoluteValue)
-                            val targetYPosition = constraints.maxHeight.toFloat() / 2f
-                            val currentYPosition = -min(targetYPosition - 24.dp.toPx(), swipeState.offsetX.value.absoluteValue * constraints.maxHeight.toFloat()/constraints.maxWidth.toFloat())
-                            if(isLikeSwipe) {
-                                translationX = currentXPosition
-                                translationY = currentYPosition
-
-                                val scale = min(2.5f, 1 + swipeState.offsetX.value.absoluteValue / constraints.maxWidth.toFloat())
-
-                                scaleX = scale
-                                scaleY = scale
-                            } else {
-                                translationY = -currentXPosition * (constraints.maxHeight.toFloat() / constraints.maxWidth)
-                            }
-                        },
-                    imageVector = Icons.Default.Favorite,
-                    buttonSize = actionButtonSize,
-                    color = Color.Green
-                )
-
-                FGCircularButton(
-                    modifier = Modifier
-                        .align(Alignment.BottomStart)
-                        .graphicsLayer {
-                            val isDismissSwipe = swipeState.offsetX.value > 0f
-                            val targetXPosition = constraints.maxWidth.toFloat() / 2f
-                            val currentXPosition = min(targetXPosition - 24.dp.toPx(), swipeState.offsetX.value.absoluteValue)
-                            val targetYPosition = constraints.maxHeight.toFloat() / 2f
-                            val currentYPosition = -min(targetYPosition - 24.dp.toPx(), swipeState.offsetX.value.absoluteValue * constraints.maxHeight.toFloat()/constraints.maxWidth.toFloat())
-                            if(isDismissSwipe) {
-                                translationX = currentXPosition
-                                translationY = currentYPosition
-
-                                val scale = min(2.5f, 1 + swipeState.offsetX.value.absoluteValue / constraints.maxWidth.toFloat())
-
-                                scaleX = scale
-                                scaleY = scale
-                            } else {
-                                translationY = currentXPosition * (constraints.maxHeight.toFloat() / constraints.maxWidth)
-                            }
-                    },
-                    imageVector = Icons.Default.Cancel,
-                    color = Color.Red
-                )
+            }
         }
     }
 
 }
 
 
-@Preview(device = Devices.NEXUS_5X)
+@Preview(device = Devices.NEXUS_5X, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun TinderPictureCardPreview() {
-    Box(
+    BoxWithConstraints(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         TinderPictureCard(
+            Modifier,
             Picture.picturesMockList[0],
+            {},
             {}
         )
     }
